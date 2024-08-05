eaf7371a72f6f18b5589f77b0e95a3d3eec73d72
Vladislav Balandin <vladbalandin2013@gmail.com>
2024-08-05 21:21:39 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import Rive from 'rive-react-native';
+
+export default function App() {
+  return (
+    <Rive
+      url="https://public.rive.app/community/runtime-files/11981-22822-hearts-test.riv"
+      artboardName="artboard"
+      stateMachineName="machine"
+    />
+  );
+}
